33749146a227149db773551babcce615886fbffb
dRamilowski <67924799+beyus723@users.noreply.github.com>
2023-03-06 13:37:27 +0000

    name numbers randomizer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import fill_list, displayList as Display
+import fill_list, displayList as Display, datetime, random
 
 continueLoop = True
 while continueLoop == True:
@@ -8,11 +8,19 @@ while continueLoop == True:
 
   Display.DisplayDetails(userDetails)
 
+  date = datetime.datetime.now()
+  print("\n", date.strftime("%d/%m/%y"))
+
   choice = input("\nAre you happy with these? Y or N")
 
   if choice.lower() == "y":
     print("Thanks...\n\n")
     continueLoop == False
+    
+    randomName = random.randint(0,9)
+    print((userDetails[0] + " ") * randomName)
+
+    
     
   elif choice.lower() == "n":
     print("\n\nTry again...\n\n")
